58eb348c8eff9ff8d196d261cd6f6caf8f0f67f1
darthvader1118 <kyellayi@gmail.com>
2016-10-19 18:42:09 -0400

    submission

--- word.js
+++ word.js
@@ -1,11 +1,18 @@
 var game = require("game.js")
 require("letter.js")
 
+function Word(word){
+  this.word = game.getWord();
+  this.letters = [];
+  for(var i = 0; i< randWord.length; i++){
+    letters.push(new Letter(randWord.charAt(i),false)) 
+}
+
+}
 
 var randWord = game.getWord();
 var letters = []
 
-for(var i = 0; i< randWord.length; i++){
-    letters.push(new Letter(randWord.charAt(i),false)) 
-}
+
+
 
